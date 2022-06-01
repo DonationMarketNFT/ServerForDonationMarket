d6b9735fbb499f8262908b12861464922f578431
downy <TeamBehindDowny@gmail.com>
2022-06-02 00:35:04 +0900

    modify content-type in s3 image upload

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -23,6 +23,10 @@ export class ImageController {
         s3: s3,
         bucket: process.env.AWS_S3_BUCKET_NAME,
         acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        // metadata: function (req, file, cb) {
+        //   cb(null, { fieldName: file.fieldname });
+        // },
         key: function (req, file, cb) {
           cb(null, file.originalname);
         },
